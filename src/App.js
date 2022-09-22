dc3d35267d7824e8f2fe6ba8ec107216075f6a09
silvia <silvia.phungky@shipper.id>
2022-09-22 09:40:26 +0700

    logic set-n

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+
+const ROWS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+]
 
 function App() {
+  const [sign, setSign] = useState('X')
+  const [cellValue, setCellValue] = useState(Array(9).fill(''))
+
+  const handleClick = ({ index }) => {
+    const updatedCellValue = [...cellValue]
+    updatedCellValue[index] = sign
+
+    setCellValue(updatedCellValue)
+    let updatedSign = sign
+
+    if (sign === 'X') {
+      updatedSign = 'O'
+    } else updatedSign = 'X'
+
+    setSign(updatedSign)
+  }
+
+  const Cell = ({ index }) => (
+    <td
+      style={{
+        border: '1px solid black',
+        textAlign: 'center',
+        width: '20px',
+        height: '20px',
+      }}
+      onClick={() => handleClick({ index })}
+    >
+      {cellValue[index] || ''}
+    </td>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table style={{ width: '100px', height: '100px' }}>
+        {ROWS.map((row, i) => (
+          <tr key={`row-${i}`}>
+            {row.map((rowIndex) => (
+              <Cell index={rowIndex} />
+            ))}
+          </tr>
+        ))}
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
